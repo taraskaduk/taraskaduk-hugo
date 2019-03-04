fb242fab14205ecdc8527e5a46738859fd4634e8
taraskaduk <taras.kdk@gmail.com>
2019-03-03 21:50:17 -0500

    more

---
+++ static/sqlsat759-pbi/demo/gapminder.R
@@ -0,0 +1,7 @@
+library(gapminder)
+library(dplyr)
+gapminder %>%
+    filter(year == '2007') %>%
+    mutate(lifeExpMonths = 12 * lifeExp) %>%
+    arrange(desc(lifeExpMonths))
+gapminder %>% View()
